37645eb8294d212588bf699b018ea12b0141439d
Olyhandi <zhilkinaolyha@gmail.com>
2024-09-26 22:43:45 +0300

    Самостоятельная работа по уроку "Распаковка позиционных параметров".
    Задача "Распаковка":
    1.Функция с параметрами по умолчанию:
    Создайте функцию print_params(a = 1, b = 'строка', c = True), которая принимает три параметра со значениями по умолчанию (например сейчас это: 1, 'строка', True).
    Функция должна выводить эти параметры.
    Вызовите функцию print_params с разным количеством аргументов, включая вызов без аргументов.
    Проверьте, работают ли вызовы print_params(b = 25) print_params(c = [1,2,3])
    2.Распаковка параметров:
    Создайте список values_list с тремя элементами разных типов.
    Создайте словарь values_dict с тремя ключами, соответствующими параметрам функции print_params, и значениями разных типов.
    Передайте values_list и values_dict в функцию print_params, используя распаковку параметров (* для списка и ** для словаря).
    3.Распаковка + отдельные параметры:
    Создайте список values_list_2 с двумя элементами разных типов
    Проверьте, работает ли print_params(*values_list_2, 42)

---
+++ module_3_3.py
@@ -0,0 +1,16 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params()
+print_params(a=34)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+value_list = [30, 'Olga', True]
+print_params(*value_list)
+
+value_dict = {'a': 6, 'b': "Мороженое", 'c': [6, 3, 9]}
+print_params(**value_dict)
+
+value_list2 = [54.32, 'Cтрока']
+print_params(*value_list2, 42)
